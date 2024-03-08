477592f452d89e09a536076bec7578bf85934193
Talha <talhasifat23@gamil.com>
2024-03-08 11:46:08 +0500

    insertion operation added

--- MyCSharpApp/Program.cs
+++ MyCSharpApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?limit=20
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using MongoDB.Bson;
@@ -11,40 +12,38 @@ class Program
 
     static async Task Main(string[] args)
     {
-        // await ProcessRepositories();
-        ConnectToMongoDB();
+        var apiData = await ProcessRepositories();
+        var database = ConnectToMongoDB();
+        InsertIntoMongoDB(database, "data", apiData);
     }
 
-    private static async Task ProcessRepositories()
+    private static async Task<string> ProcessRepositories()
     {
         client.DefaultRequestHeaders.Accept.Clear();
         client.DefaultRequestHeaders.Accept.Add(
             new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
-        var stringTask = client.GetStringAsync("http://api.github.com/orgs/dotnet/repos");
+        var stringTask = client.GetStringAsync("https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?limit=20");
 
         var msg = await stringTask;
-        Console.Write(msg);
+        return msg;
     }
 
-    private static void ConnectToMongoDB()
+    private static IMongoDatabase ConnectToMongoDB()
     {
         var connectionString = "mongodb://localhost:27017";
         mongoClient = new MongoClient(connectionString);
-        var database = mongoClient.GetDatabase("appdb");
-        var collection = database.GetCollection<BsonDocument>("users");
-        
-
-        var firstDocument = collection.Find(new BsonDocument()).FirstOrDefault();
-        if (firstDocument != null)
-        {
-            Console.WriteLine(firstDocument.ToString());
-        }
-        else
-        {
-            Console.WriteLine("No documents found.");
-        }
+        var database = mongoClient.GetDatabase("mydb");
 
         Console.WriteLine("Connected to MongoDB!");
+        return database;
+    }
+
+    private static void InsertIntoMongoDB(IMongoDatabase database, string collectionName, string data)
+    {
+        var collection = database.GetCollection<BsonDocument>(collectionName);
+        var document = BsonDocument.Parse(data);
+        collection.InsertOne(document);
+        Console.WriteLine("Data inserted into MongoDB!");
     }
 }
