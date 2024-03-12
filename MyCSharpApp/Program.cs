4c20767e0922b26d56763bcbe44d4fc8cb38fef8
Talha <talhasifat23@gmail.com>
2024-03-12 22:04:17 +0500

    read documents and ex handling

--- MyCSharpApp/Program.cs
+++ MyCSharpApp/Program.cs
@@ -5,16 +5,28 @@ using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
+
+
 class Program
 {
+
+
     private static readonly HttpClient client = new HttpClient();
     private static MongoClient mongoClient = new MongoClient();
 
     static async Task Main(string[] args)
     {
-        var apiData = await ProcessRepositories();
-        var database = ConnectToMongoDB();
-        InsertIntoMongoDB(database, "data", apiData);
+        try
+        {
+            var database = ConnectToMongoDB();
+            var apiData = await ProcessRepositories();
+            InsertIntoMongoDB(database, "data", apiData);
+            ReadAllDocuments(database, "data");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
     }
 
     private static async Task<string> ProcessRepositories()
@@ -23,10 +35,9 @@ class Program
         client.DefaultRequestHeaders.Accept.Add(
             new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
-        var stringTask = client.GetStringAsync("https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?limit=20");
-
-        var msg = await stringTask;
-        return msg;
+        var response = await client.GetStringAsync("https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?limit=20");
+        var data = response;
+        return data;
     }
 
     private static IMongoDatabase ConnectToMongoDB()
@@ -34,7 +45,6 @@ class Program
         var connectionString = "mongodb://localhost:27017";
         mongoClient = new MongoClient(connectionString);
         var database = mongoClient.GetDatabase("mydb");
-
         Console.WriteLine("Connected to MongoDB!");
         return database;
     }
@@ -45,5 +55,15 @@ class Program
         var document = BsonDocument.Parse(data);
         collection.InsertOne(document);
         Console.WriteLine("Data inserted into MongoDB!");
+    }
+    private static void ReadAllDocuments(IMongoDatabase database, string collectionName)
+    {
+        var collection = database.GetCollection<BsonDocument>(collectionName);
+        var documents = collection.Find(new BsonDocument()).ToList();
+
+        foreach (var document in documents)
+        {
+            Console.WriteLine(document.ToString());
+        }
     }
 }
